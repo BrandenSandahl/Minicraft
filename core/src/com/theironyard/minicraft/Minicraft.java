63fdcef09a6eb22ce76bee0716b3204dfe5f8ba6
BrandenSandahl <branden157@gmail.com>
2016-02-18 14:51:35 -0500

    added u,d,l,r turns and world wrap at boundries

--- core/src/com/theironyard/minicraft/Minicraft.java
+++ core/src/com/theironyard/minicraft/Minicraft.java
@@ -2,24 +2,36 @@ package com.theironyard.minicraft;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.sun.prism.image.ViewPort;
 
 public class Minicraft extends ApplicationAdapter {
     final int WIDTH = 100;
     final int HEIGHT = 100;
+    final float MAXVELOCITY = 200;
+
+    FitViewport view;
+
 
     SpriteBatch batch;
-    TextureRegion down, up, right, left;
+    TextureRegion down, up, right, left, direction;
 
-    float x = 0;
-    float y = 0;
+    float x, y, xv, yv;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
+
+        view = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //these calls get width and height
+
 
         Texture tiles = new Texture("tiles.png");
         TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
@@ -28,15 +40,71 @@ public class Minicraft extends ApplicationAdapter {
         right = grid[6][3];
         left = new TextureRegion(right);
         left.flip(true, false);
+        direction = down;
     }
 
     @Override
     public void render () {
         Gdx.gl.glClearColor(0, 0.5f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        direction = move();
+
+        if (y < (HEIGHT * -1) ) {
+            y = Gdx.graphics.getHeight();
+        }
+        if (y > Gdx.graphics.getHeight()) {
+            y = (0 - HEIGHT);
+        }
+        if (x < (WIDTH * -1)) {
+            x = Gdx.graphics.getWidth();
+        }
+        if ( x > Gdx.graphics.getWidth()) {
+            x = (0 - WIDTH);
+        }
 
         batch.begin();
-        batch.draw(down, x, y, WIDTH, HEIGHT);
+        batch.draw(this.direction, x, y, WIDTH, HEIGHT);
         batch.end();
     }
+
+
+    public float decelerate(float velocity ) {
+
+        float deceleration = 0.8f; //closer to 1, slower the decel
+        velocity *= deceleration;
+        if (Math.abs(velocity) < 1) {
+            velocity = 0;
+        }
+        return velocity;
+    }
+
+    public TextureRegion move() {
+        //grabbing a keystroke
+        TextureRegion directionMove = direction;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            yv = MAXVELOCITY;
+            directionMove = up;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            yv = MAXVELOCITY * -1;
+            directionMove = down;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            xv = MAXVELOCITY;
+            directionMove = right;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            xv = MAXVELOCITY * -1;
+            directionMove = left;
+        }
+
+        y += yv * Gdx.graphics.getDeltaTime();
+        yv = decelerate(yv);
+        x += xv * Gdx.graphics.getDeltaTime();
+        xv = decelerate(xv);
+
+        return directionMove;
+    }
+
+
 }
